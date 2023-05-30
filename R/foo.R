3867cca014bc8db404142304b2b360fa4edc5e66
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-05-30 09:29:33 +0800

    Test

---
+++ R/foo.R
@@ -0,0 +1,24 @@
+#' @title Sample Function
+#'
+#' @description Sample function.
+#'
+#' @details Sample function.
+#'
+#' @return
+#' The value of `arg1`.
+#'
+#' @param arg1 Argument description.
+#' @param ... Additional arguments.
+#'
+#' @author Shu Fai Cheung <https://orcid.org/0000-0002-9871-9448>
+#'
+#' @examples
+#' library(psych)
+#'
+#' @export
+#'
+#
+
+foo <- function(arg1 = 0, ...) {
+    arg1
+  }
---
+++ tests/testthat/test_foo.R
@@ -0,0 +1,8 @@
+
+library(testthat)
+library(psych)
+
+x <- 10
+test_that("foo", {
+    expect_equal(foo(arg1 = 10), 10)
+  })
